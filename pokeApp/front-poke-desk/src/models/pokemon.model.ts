5cad0cce9c8fb908d9efa382cb67dd296a12f891
Filipe <llyphy46@gmail.com>
2022-04-09 01:18:54 -0300

    adicionando models

---
+++ pokeApp/front-poke-desk/src/models/pokemon.model.ts
@@ -0,0 +1,10 @@
+export interface Pokemon {
+    Name: string,
+    Pokedex_Number: number,
+    Type_1: string,
+    Type_2: string,
+    STAT_TOTAL: string,
+    ATK: string,
+    DEF: string,
+    STA: string
+  }
